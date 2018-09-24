9152fb2caa73a2115a225e1a48385bce54eaa3cb
André Roaldseth <andre@roaldseth.net>
2018-09-24 15:31:42 +0200

    Set up as a standalone project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,22 @@
 plugins {
-    kotlin("jvm")
     application
+    kotlin("jvm") version "1.2.51"
+    id("com.diffplug.gradle.spotless") version "3.13.0"
+    id("com.palantir.docker") version "0.20.1"
+    id("com.palantir.git-version") version "0.11.0"
+}
+
+apply {
+    plugin("com.diffplug.gradle.spotless")
+}
+
+repositories {
+    jcenter()
+    maven(url = "http://packages.confluent.io/maven/")
 }
 
 application {
-    version = "0.0.1"
-    applicationName = "joark-mottak"
+    applicationName = "dagpenger-joark-mottak"
     mainClassName = "no.nav.dagpenger.joark.mottak.JoarkMottak"
 }
 
@@ -13,12 +24,22 @@ val kotlinLoggingVersion = "1.4.9"
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation(project(":events"))
-    implementation(project(":streams"))
+    implementation("dagpenger:events")
+    implementation("dagpenger:streams")
 
     implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
 
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
     testImplementation("junit:junit:4.12")
+}
+
+spotless {
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        target("*.gradle.kts", "additionalScripts/*.gradle.kts")
+        ktlint()
+    }
 }
