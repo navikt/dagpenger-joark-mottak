7b1fb55ead7467b579882c0cf508911a4bf74471
André Roaldseth <andre.roaldseth@nav.no>
2018-09-25 10:23:01 +0200

    Remove unreachable code

--- src/main/kotlin/no/nav/dagpenger/joark/mottak/JournalpostArkivDummy.kt
+++ src/main/kotlin/no/nav/dagpenger/joark/mottak/JournalpostArkivDummy.kt
@@ -3,6 +3,5 @@ package no.nav.dagpenger.joark.mottak
 class JournalpostArkivDummy : JournalpostArkiv {
     override fun hentInngåendeJournalpost(journalpostId: String): String {
         return "bar"
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
