fba96141466f8817422473773ec1b0a56779f8be
Geir André Lund <geir.andre.lund@nav.no>
2018-09-28 15:11:33 +0200

    "OpenID connect" client for fetching access token to be used against joark journal post endpoint DIGIDAG-148

---
+++ src/test/kotlin/no/nav/dagpenger/joark/mottak/StsOidcClientTest.kt
@@ -0,0 +1,83 @@
+package no.nav.dagpenger.joark.mottak
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.exactly
+import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.verify
+import com.github.tomakehurst.wiremock.matching.RegexPattern
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class StsOidcClientTest {
+    companion object {
+        val wireMockServer = WireMockServer()
+
+        @BeforeClass
+        @JvmStatic
+        fun setup() {
+            wireMockServer.start()
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun after() {
+            wireMockServer.stop()
+        }
+    }
+
+    private val expires = 300L
+
+    @Test
+    fun `fetch open id token from sts server`() {
+
+        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/stsurl?grant_type=client_credentials&scope=openid"))
+                .withHeader("Authorization", RegexPattern("Basic\\s[a-zA-Z0-9]*="))
+                .willReturn(WireMock.aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(body())
+                )
+        )
+
+        val stsOidcClient = StsOidcClient(wireMockServer.url("stsurl"), "username", "password")
+        val oidcToken: OidcToken = stsOidcClient.oidcToken()
+
+        assertEquals(oidcToken, OidcToken("token", "openid", expires))
+    }
+
+    @Test
+    fun `fetch open id token from sts server and token is cached `() {
+
+        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/cached?grant_type=client_credentials&scope=openid"))
+                .withHeader("Authorization", RegexPattern("Basic\\s[a-zA-Z0-9]*="))
+                .willReturn(WireMock.aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(body())
+                )
+        )
+        val stsOidcClient = StsOidcClient(wireMockServer.url("cached"), "username", "password")
+
+        val firstCall: OidcToken = stsOidcClient.oidcToken()
+
+        assertEquals(firstCall, OidcToken("token", "openid", expires))
+
+        val secondCall: OidcToken = stsOidcClient.oidcToken()
+
+        assertEquals(secondCall, OidcToken("token", "openid", expires))
+
+        verify(exactly(1), getRequestedFor(urlEqualTo("/stsurl?grant_type=client_credentials&scope=openid"))
+                .withHeader("Authorization", RegexPattern("Basic\\s[a-zA-Z0-9]*=")))
+    }
+
+    fun body() = """
+                {
+                    "access_token": "token",
+                    "token_type" : "openid",
+                    "expires_in" : $expires
+                }
+
+            """.trimIndent()
+}
