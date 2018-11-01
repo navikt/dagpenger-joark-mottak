d236569ee9e3239d2873a0840ed52b8cd8654f73
Geir André Lund <geir.andre.lund@nav.no>
2018-11-01 13:19:05 +0100

    Use fasit environment name to determine kafka topic name for now

--- src/main/kotlin/no/nav/dagpenger/joark/mottak/Environment.kt
+++ src/main/kotlin/no/nav/dagpenger/joark/mottak/Environment.kt
@@ -6,7 +6,8 @@ data class Environment(
     val oicdStsUrl: String = getEnvVar("OIDC_STS_ISSUERURL"),
     val journalfoerinngaaendeV1Url: String = getEnvVar("JOURNALFOERINNGAAENDE_V1_URL"),
     val bootstrapServersUrl: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092"),
-    val schemaRegistryUrl: String = getEnvVar("KAFKA_SCHEMA_REGISTRY_URL", "localhost:8081")
+    val schemaRegistryUrl: String = getEnvVar("KAFKA_SCHEMA_REGISTRY_URL", "localhost:8081"),
+    val fasitEnvironmentName: String = getEnvVar("FASIT_ENVIRONMENT_NAME", "")
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
