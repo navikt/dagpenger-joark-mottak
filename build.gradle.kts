a94c525535cefccbf96778b833f36205f0ebc372
Geir André Lund <geir.andre.lund@nav.no>
2018-11-09 10:41:20 +0100

    Wiremock to 2.19.0 and pitest version to 1.4.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,7 +80,6 @@ dependencies {
     testImplementation(kotlin("test-junit"))
     testImplementation("junit:junit:4.12")
     testImplementation("com.github.tomakehurst:wiremock:2.19.0")
-
     testImplementation("no.nav:kafka-embedded-env:2.0.1")
 }
 
