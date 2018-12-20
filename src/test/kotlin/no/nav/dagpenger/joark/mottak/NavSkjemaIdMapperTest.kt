88b3549e756dd2a26dcdfb78b0c69b6b5db81ed5
André Roaldseth <andre.roaldseth@nav.no>
2018-12-20 15:51:11 +0100

    Refactor out nested object

---
+++ src/test/kotlin/no/nav/dagpenger/joark/mottak/NavSkjemaIdMapperTest.kt
@@ -0,0 +1,27 @@
+package no.nav.dagpenger.joark.mottak
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class NavSkjemaIdMapperTest {
+    @Test
+    fun ` known NETS id gets mapped to correct skjemaId`() {
+        val type = NavSkjemaIdMapper.getNavSkjemaId("196002")
+
+        assertEquals(type, "NAV 04-01.03")
+    }
+
+    @Test
+    fun ` unknown NETS id gets mapped to Unknown`() {
+        val type = NavSkjemaIdMapper.getNavSkjemaId("xxx")
+
+        assertEquals(type, "Unknown")
+    }
+
+    @Test
+    fun ` no NETS id gets mapped to Unknown`() {
+        val type = NavSkjemaIdMapper.getNavSkjemaId("xxx")
+
+        assertEquals(type, "Unknown")
+    }
+}
