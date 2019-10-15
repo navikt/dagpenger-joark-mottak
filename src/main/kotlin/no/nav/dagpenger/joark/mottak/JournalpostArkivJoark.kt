d2559da75fad21f45dfe18138952dc75acc03051
Geir André Lund <geir.andre.lund@nav.no>
2019-10-15 09:01:52 +0200

    Trigger build

--- src/main/kotlin/no/nav/dagpenger/joark/mottak/JournalpostArkivJoark.kt
+++ src/main/kotlin/no/nav/dagpenger/joark/mottak/JournalpostArkivJoark.kt
@@ -1,5 +1,6 @@
 package no.nav.dagpenger.joark.mottak
 
+import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.gson.responseObject
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
@@ -14,7 +15,7 @@ class JournalpostArkivJoark(private val joarkBaseUrl: String, private val oidcCl
     override fun hentInngåendeJournalpost(journalpostId: String): Journalpost {
         val url = "$joarkUrl/$journalpostId"
         val (_, response, result) = with(url.httpGet()) {
-            header("Authorization" to oidcClient.oidcToken().access_token.toBearerToken())
+            authentication().bearer(oidcClient.oidcToken().access_token)
             responseObject<Journalpost>()
         }
         return when (result) {
@@ -27,8 +28,6 @@ class JournalpostArkivJoark(private val joarkBaseUrl: String, private val oidcCl
         }
     }
 }
-
-fun String.toBearerToken() = "Bearer $this"
 
 class JournalpostArkivException(val statusCode: Int, override val message: String, override val cause: Throwable) :
     RuntimeException(message, cause)
