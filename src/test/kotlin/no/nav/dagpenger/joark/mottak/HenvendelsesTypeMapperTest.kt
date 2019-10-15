178cd06027ae796b9adce5c92b731db1f2cd0956
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-10-15 08:50:05 +0200

    Update test to match henvendelsestype-enum

--- src/test/kotlin/no/nav/dagpenger/joark/mottak/HenvendelsesTypeMapperTest.kt
+++ src/test/kotlin/no/nav/dagpenger/joark/mottak/HenvendelsesTypeMapperTest.kt
@@ -1,31 +1,28 @@
 package no.nav.dagpenger.joark.mottak
 
-import no.nav.dagpenger.events.avro.Annet
-import no.nav.dagpenger.events.avro.Søknad
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
-
 import org.junit.jupiter.api.Test
 
 class HenvendelsesTypeMapperTest {
 
     @Test
-    fun ` known skjemaId gets mapped to correct type`() {
+    fun ` known søknad-skjemaId gets mapped to søknad`() {
         val type = HenvendelsesTypeMapper.getHenvendelsesType("NAV 04-01.03")
-        assertEquals(type, Søknad())
+        assertEquals(type, Henvendelsestype.SØKNAD)
     }
 
     @Test
     fun ` unknown skjemaId gets mapped to Annet`() {
         val type = HenvendelsesTypeMapper.getHenvendelsesType("xxx")
-        assertEquals(type, Annet())
+        assertEquals(type, Henvendelsestype.ANNET)
     }
 
     @Test
     fun ` no skjemaId gets mapped to Annet`() {
         val type = HenvendelsesTypeMapper.getHenvendelsesType(null)
-        assertEquals(type, Annet())
+        assertEquals(type, Henvendelsestype.ANNET)
     }
 
     @Test
