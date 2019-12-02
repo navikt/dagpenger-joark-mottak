1ad99df4c77d40809d8d6c307d8e4a5f72479821
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-12-02 12:25:26 +0100

    la tittlene for brevkodene over i allKnownTypes
    Gjorde dette for å ikke duplisere. Brevkoder og titler er basert på
    https://tjenester.nav.no/soknadsveiviserproxy/skjemautlisting filtrert
    på tema=DAG. Denne inneholder derimot ikke ettersendelser
    (NAVe-brevkoder), disse fant jeg dermed her:
    https://confluence.adeo.no/pages/viewpage.action?pageId=294131690#Nyttinnl%C3%B8p/nyrutingforDagpenger-Oversiktoveralleskjema

--- src/main/kotlin/no/nav/dagpenger/joark/mottak/Journalpost.kt
+++ src/main/kotlin/no/nav/dagpenger/joark/mottak/Journalpost.kt
@@ -23,7 +23,7 @@ data class Journalpost(
 
 class DokumentInfo(tittel: String?, dokumentInfoId: String, brevkode: String?) {
     val tittel = tittel
-        get() = field ?: tittelForBrevkode.getOrDefault(brevkode, "Ukjent dokumenttittel")
+        get() = field ?: HenvendelsesTypeMapper.allKnownTypes.getOrDefault(brevkode, "Ukjent dokumenttittel")
     val dokumentInfoId = dokumentInfoId
     val brevkode = brevkode
 }
@@ -57,37 +57,3 @@ enum class Journalstatus {
     UKJENT_BRUKER, RESERVERT, OPPLASTING_DOKUMENT,
     UKJENT
 }
-
-val tittelForBrevkode = mapOf(
-    "NAV 04-02.03" to "Bekreftelse på ansettelsesforhold",
-    "O2" to "Arbeidsavtale",
-    "M6" to "Timelister",
-    "M7" to " Brev fra bobestyrer/konkursforvalter",
-    "NAV 04-02.05" to "Søknad om attest PD U1/N-301 til bruk ved overføring av dagpengerettigheter",
-    "NAV 04-08.03" to "Bekreftelse på sluttårsak/nedsatt arbeidstid (ikke permittert)",
-    "S7" to "Kopi av arbeidsavtale/sluttårsak",
-    "NAV 04-08.04" to "Bekreftelse på arbeidsforhold og permittering",
-    "T3" to "Tjenestebevis",
-    "S6" to "Dokumentasjon av sluttårsak",
-    "NAV 04-16.04" to "Søknad om gjenopptak av dagpenger ved permittering",
-    "NAV 04-02.01" to "Søknad om utstedelse av attest PD U2",
-    "O9" to "Bekreftelse fra studiested/skole",
-    "NAV 04-06.05" to "Søknad om godkjenning av utdanning med rett til dagpenger",
-    "N2" to "Kopi av søknad",
-    "N5" to "Kopi av undersøkelsesresultat",
-    "NAV 04-06.08" to "Søknad om dagpenger under etablering av egen virksomhet (utviklingsfase)",
-    "NAV 04-16.03" to "Søknad om gjenopptak av dagpenger",
-    "NAV 04-13.01" to "Egenerklæringsskjema for fastsettelse av grensearbeiderstatus",
-    "NAV 04-03.07" to "Egenerklæring - overdragelse av lønnskrav",
-    "X8" to "Fødselsattest/bostedsbevis for barn under 18 år",
-    "T5" to "SED U006 Familieinformasjon",
-    "T4" to "Oppholds- og arbeidstillatelse, eller registreringsbevis for EØS-borgere",
-    "T2" to "Dokumentasjon av sluttdato",
-    "T1" to "Elevdokumentasjon fra lærested",
-    "V6" to "Kopi av sluttavtale",
-    "U1" to "U1 Perioder av betydning for retten til dagpenger",
-    "NAV 04-03.08" to "Oversikt over arbeidstimer",
-    "S8" to "Sjøfartsbok/hyreavregning",
-    "NAV 04-01.03" to "Søknad om dagpenger (ikke permittert)",
-    "NAV 04-01.04" to "Søknad om dagpenger ved permittering"
-)
