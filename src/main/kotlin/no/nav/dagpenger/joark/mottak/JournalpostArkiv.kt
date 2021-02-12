c28612aa8426fc5c4ec9ee2d3439379238bf0a3d
Geir André Lund <geir.andre.lund@nav.no>
2021-02-12 15:30:44 +0100

    Skippe journalpost i DEV

--- src/main/kotlin/no/nav/dagpenger/joark/mottak/JournalpostArkiv.kt
+++ src/main/kotlin/no/nav/dagpenger/joark/mottak/JournalpostArkiv.kt
@@ -1,6 +1,10 @@
 package no.nav.dagpenger.joark.mottak
 
+import mu.KotlinLogging
 import no.nav.dagpenger.streams.HealthCheck
+
+private val logger = KotlinLogging.logger { }
+private val sikkerLogger = KotlinLogging.logger("tjenestekall")
 
 interface JournalpostArkiv : HealthCheck {
     fun hentInngåendeJournalpost(journalpostId: String): Journalpost
@@ -18,8 +22,14 @@ data class Søknadsdata(
     }
 
     fun toMap(): Map<String, Any?>? {
-        return jsonMapAdapter.fromJson(data)?.toMap()?.let {
-            it + mapOf("journalpostId" to journalpostId, "journalRegistrertDato" to registrertDato)
+        return try {
+            jsonMapAdapter.fromJson(data)?.toMap()?.let {
+                it + mapOf("journalpostId" to journalpostId, "journalRegistrertDato" to registrertDato)
+            }
+        } catch (e: Exception) {
+            logger.info { "Klarte ikke å lese søknad med journalpost id $journalpostId" }
+            sikkerLogger.info { "Klarte ikke å lese søknad med journalpost id $journalpostId, data $data" }
+            null
         }
     }
 }
