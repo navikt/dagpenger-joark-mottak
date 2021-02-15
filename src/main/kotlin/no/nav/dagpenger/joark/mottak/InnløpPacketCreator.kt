78f7fd8356a203e0a9ee2adfc4c2d3f2cc599333
Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
2021-02-15 12:01:58 +0100

    Har også rutet feil ettersendelser til søknad om gjenopptak uten permittering.
    navikt/dagpenger#680

--- src/main/kotlin/no/nav/dagpenger/joark/mottak/InnløpPacketCreator.kt
+++ src/main/kotlin/no/nav/dagpenger/joark/mottak/InnløpPacketCreator.kt
@@ -61,7 +61,6 @@ class InnløpPacketCreator(
         listOf(
             "NAV 04-01.04",
             "NAVe 04-01.04",
-            "NAVe 04-16.03",
             "NAV 04-16.04",
             "NAVe 04-16.04",
             "NAVe 04-08.04",
