e6b49da32b58386cf44fb68c26c65168a91ef191
Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
2021-03-16 10:31:37 +0100

    Feilhåndering
    ikke commit når noe krasjer i konsumering
    
    navikt/dagpenger#692
    
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Knut Magne Riise <knut.magne.riise@nav.no>

--- src/main/kotlin/no/nav/dagpenger/joark/mottak/JoarkAivenMottak.kt
+++ src/main/kotlin/no/nav/dagpenger/joark/mottak/JoarkAivenMottak.kt
@@ -3,17 +3,29 @@ package no.nav.dagpenger.joark.mottak
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import mu.KotlinLogging
 import no.nav.dagpenger.plain.consumerConfig
 import no.nav.dagpenger.streams.KafkaCredential
+import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.errors.WakeupException
+import org.apache.kafka.common.security.auth.SecurityProtocol
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
 import java.time.Duration
+import java.util.Properties
 import kotlin.coroutines.CoroutineContext
+
+private val logger = KotlinLogging.logger {}
 
 fun consumer(bootstrapServerUrl: String, credential: KafkaCredential): KafkaConsumer<String, String> {
     return KafkaConsumer<String, String>(
@@ -24,6 +36,7 @@ fun consumer(bootstrapServerUrl: String, credential: KafkaCredential): KafkaCons
         ).also {
             it[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
             it[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+            it[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = "false"
         }
     ).also {
         it.subscribe(
@@ -33,6 +46,25 @@ fun consumer(bootstrapServerUrl: String, credential: KafkaCredential): KafkaCons
             )
         )
     }
+}
+
+private fun createAivenProducer(env: Map<String, String>): KafkaProducer<String, String>? {
+    val properties = Properties().apply {
+        put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, env.getValue("KAFKA_BROKERS"))
+        put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name)
+        put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "")
+        put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "jks")
+        put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
+        put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, env.getValue("KAFKA_TRUSTSTORE_PATH"))
+        put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, env.getValue("KAFKA_CREDSTORE_PASSWORD"))
+        put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, env.getValue("KAFKA_KEYSTORE_PATH"))
+        put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, env.getValue("KAFKA_CREDSTORE_PASSWORD"))
+
+        put(ProducerConfig.ACKS_CONFIG, "1")
+        put(ProducerConfig.LINGER_MS_CONFIG, "0")
+        put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1")
+    }
+    return KafkaProducer(properties, StringSerializer(), StringSerializer())
 }
 
 class JoarkAivenMottak(
@@ -50,29 +82,36 @@ class JoarkAivenMottak(
         }
     }
 
-    fun stop() {
+    suspend fun stop() {
+        consumer.wakeup()
+        producer.flush()
+        producer.close()
+        delay(3000)
         job.cancel()
     }
 
+    val aivenTopic = mapOf(
+        configuration.kafka.søknadsdataTopic.name to "teamdagpenger.soknadsdata.v1",
+        configuration.kafka.dagpengerJournalpostTopic.name to "teamdagpenger.journalforing.v1"
+    )
+
     private fun run() {
-        while (job.isActive) {
-            consumer.poll(Duration.ofMillis(500))
-                .forEach {
-                    when (it.topic()) {
-                        configuration.kafka.søknadsdataTopic.name -> producer.send(
-                            ProducerRecord(
-                                "teamdagpenger.soknadsdata.v1",
-                                it.value()
-                            )
-                        )
-                        configuration.kafka.dagpengerJournalpostTopic.name -> producer.send(
-                            ProducerRecord(
-                                "teamdagpenger.journalforing.v1",
-                                it.value()
-                            )
-                        )
+        try {
+            while (job.isActive) {
+                consumer.poll(Duration.ofMillis(500))
+                    .forEach {
+                        val aivenTopic = requireNotNull(aivenTopic[it.topic()])
+                        producer.send(ProducerRecord(aivenTopic, it.value()))
                     }
-                }
+                consumer.commitSync()
+            }
+        } catch (e: WakeupException) {
+            logger.info(e) { "Consumeren stenges" }
+        } catch (e: Exception) {
+            logger.error(e) { "Noe feil skjedde" }
+            throw e
+        } finally {
+            consumer.close()
         }
     }
 }
--- src/test/kotlin/no/nav/dagpenger/joark/mottak/JoarkAivenMottakTest.kt
+++ src/test/kotlin/no/nav/dagpenger/joark/mottak/JoarkAivenMottakTest.kt
@@ -11,29 +11,40 @@ import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.common.TopicPartition
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.concurrent.Future
 
 class JoarkAivenMottakTest {
-    @Test
-    fun `greier å lese topic`() {
-        val journalpostMottattTopic = "privat-dagpenger-journalpost-mottatt-v1"
-        val søknadsdataTopic = "privat-dagpenger-soknadsdata-v1"
+    val journalpostMottattTopic = "privat-dagpenger-journalpost-mottatt-v1"
+    val søknadsdataTopic = "privat-dagpenger-soknadsdata-v1"
+    val topicPartition1 = TopicPartition(journalpostMottattTopic, 1)
+    val topicPartition2 = TopicPartition(søknadsdataTopic, 1)
 
-        val mockConsumer = MockConsumer<String, String>(OffsetResetStrategy.EARLIEST).also {
-            val topicPartition1 = TopicPartition(journalpostMottattTopic, 1)
-            val topicPartition2 = TopicPartition(søknadsdataTopic, 1)
-            it.assign(listOf(topicPartition1, topicPartition2))
-            it.updateBeginningOffsets(
-                mapOf(
-                    topicPartition1 to 0L,
-                    topicPartition2 to 0L
-                )
+    val mockConsumer = MockConsumer<String, String>(OffsetResetStrategy.EARLIEST).also {
+        it.assign(listOf(topicPartition1, topicPartition2))
+        it.updateBeginningOffsets(
+            mapOf(
+                topicPartition1 to 0L,
+                topicPartition2 to 0L
             )
-        }
-        val mockProducer = mockk<Producer<String, String>>()
-        val recordSlots = mutableListOf<ProducerRecord<String, String>>()
+        )
+    }
+    val recordSlots = mutableListOf<ProducerRecord<String, String>>()
+    val mockProducer = mockk<Producer<String, String>>()
 
+    @BeforeEach
+    fun reset() {
+        mockConsumer.updateBeginningOffsets(
+            mapOf(
+                topicPartition1 to 0L,
+                topicPartition2 to 0L
+            )
+        )
+    }
+
+    @Test
+    fun `sender til riktig topic`() {
         coEvery { mockProducer.send(capture(recordSlots)) } returns mockk<Future<RecordMetadata>>()
 
         JoarkAivenMottak(
@@ -52,5 +63,26 @@ class JoarkAivenMottakTest {
             it.last().topic() shouldBe "teamdagpenger.soknadsdata.v1"
             it.last().value() shouldBe "søknadsverdi"
         }
+        val offsetData = mockConsumer.committed(setOf(topicPartition1, topicPartition2))
+        offsetData[topicPartition1]?.offset() shouldBe 1L
+        offsetData[topicPartition2]?.offset() shouldBe 1L
+    }
+
+    @Test
+    fun `committer ikke når det skjer feil i konsumering`() {
+        coEvery { mockProducer.send(any()) } throws RuntimeException()
+
+        JoarkAivenMottak(
+            mockConsumer,
+            mockProducer,
+            Configuration()
+        ).start()
+
+        mockConsumer.addRecord(ConsumerRecord(journalpostMottattTopic, 1, 0, "key", "enverdi"))
+        mockConsumer.addRecord(ConsumerRecord(søknadsdataTopic, 1, 0, "key", "søknadsverdi"))
+
+        val offsetData = mockConsumer.committed(setOf(topicPartition1, topicPartition2))
+        offsetData[topicPartition1]?.offset() shouldBe null
+        offsetData[topicPartition2]?.offset() shouldBe null
     }
 }
