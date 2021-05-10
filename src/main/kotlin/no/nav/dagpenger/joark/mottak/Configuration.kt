144938bdd4ab062c3b70b0f2105e833e4bfc6081
Giao The Cung <giaothe@gmail.com>
2021-05-10 09:58:29 +0200

    Konfigurasjon for gcp kluster

--- src/main/kotlin/no/nav/dagpenger/joark/mottak/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/joark/mottak/Configuration.kt
@@ -45,6 +45,8 @@ private val prodProperties = ConfigurationMap(
 
 private fun config() = when (System.getenv("NAIS_CLUSTER_NAME") ?: System.getProperty("NAIS_CLUSTER_NAME")) {
     "dev-fss" -> systemProperties() overriding EnvironmentVariables overriding devProperties
+    "dev-gcp" -> systemProperties() overriding EnvironmentVariables overriding devProperties
+    "prod-gcp" -> systemProperties() overriding EnvironmentVariables overriding prodProperties
     "prod-fss" -> systemProperties() overriding EnvironmentVariables overriding prodProperties
     else -> {
         systemProperties() overriding EnvironmentVariables overriding localProperties
