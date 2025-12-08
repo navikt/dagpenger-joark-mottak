d6a29ae376ec1df2434214ac23e08180d689098e
Giao The Cung <giaothe@gmail.com>
2025-12-08 11:55:59 +0100

    Logger litt mer

--- src/main/kotlin/no/nav/dagpenger/joark/mottak/JoarkMottak.kt
+++ src/main/kotlin/no/nav/dagpenger/joark/mottak/JoarkMottak.kt
@@ -1,5 +1,6 @@
 package no.nav.dagpenger.joark.mottak
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.install
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
@@ -7,9 +8,14 @@ import io.ktor.server.plugins.defaultheaders.DefaultHeaders
 import io.ktor.server.routing.routing
 import no.nav.dagpenger.joark.mottak.KafkaConfig.aivenProducer
 import no.nav.dagpenger.joark.mottak.KafkaConfig.joarkAivenConsumer
+import kotlin.math.log
 
+private val logger = KotlinLogging.logger {}
 fun main() {
     val config = Configuration()
+
+    logger.info { "Starter JoarkMottak" }
+
     val aivenJournalfoeringReplicator =
         JournalfoeringReplicator(
             joarkAivenConsumer(
